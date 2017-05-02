a1ad3bf471418680b058558c6a6c85b8613860e9
Thomas Thous <tthous@hawk.iit.edu>
2017-05-02 18:11:53 -0400

    Changed to plasma colormap for readability (magma is good for filtering objects with low reflectivity)

--- test2.py
+++ test2.py
@@ -8,7 +8,7 @@ import mpld3
 fig = pylab.figure()
 ax = Axes3D(fig)
 
-v = 100
+v = 80
 
 
 data = np.genfromtxt('pointcloud1.fuse')
@@ -17,5 +17,5 @@ y = data[:,1]
 z = data[:,2]
 color = data[:,3]
 
-scatter = ax.scatter(x[0::v] ,y[0::v] ,z[0::v], c=color[0::v])
+scatter = ax.scatter(x[0::v] ,y[0::v] ,z[0::v], c=color[0::v], cmap='plasma')
 pyplot.show()
